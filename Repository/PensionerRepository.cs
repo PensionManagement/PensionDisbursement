72dddcbfd1cb33689d64f8dc8c1e08639d61a6f1
Akash.Paramasivan <akash.paramasivan@kanini.com>
2021-04-20 21:57:50 +0530

    Add project files.

---
+++ Repository/PensionerRepository.cs
@@ -0,0 +1,77 @@
+﻿using PensionDisbursement.Models;
+using PensionDisbursement.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PensionDisbursement.Repository
+{
+    public class PensionerRepository:IPensionerRepository
+    {
+        private readonly IPensionerProvider _provider;
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(PensionerRepository));
+        ProcessPensionResponse processPensionResponse = new ProcessPensionResponse();
+        public double amount,finalamount;
+        public PensionerRepository(IPensionerProvider provider)
+        {
+            _provider = provider;
+        }
+        public PensionerDetail GetDetail(string AadhaarNumber)
+        {
+            PensionerDetail pensionerDetail = null;
+
+            _log4net.Info("Requesting for Info");
+
+
+
+            pensionerDetail = _provider.GetData(AadhaarNumber);
+            _log4net.Info("Information Fetched");
+            return pensionerDetail;
+        }
+
+        public int status(PensionerDetail pensionerDetail, ProcessPensionInput input)
+        {
+            if (input.PensionAmount == 0)
+                return 21;
+            
+            if ( pensionerDetail.PensionType == 1 )
+            {
+               amount = (double)pensionerDetail.Salary*0.8;
+
+                
+            }
+            else if(pensionerDetail.PensionType == 2)
+            {
+                amount = (double)pensionerDetail.Salary * 0.5;
+            }
+            if(pensionerDetail.BankType==1)
+            {
+                finalamount = amount - 500;
+            }
+            else if (pensionerDetail.BankType == 2)
+            {
+                finalamount = amount -550;
+            }
+
+            if (input.PensionAmount==finalamount)
+            {
+                return 10;
+            }
+           
+            else if (input.PensionAmount==amount&&pensionerDetail.BankType==1)
+            {
+                return 20;
+            }
+            else if(input.PensionAmount == amount&& pensionerDetail.BankType == 2)
+            {
+                return 20;
+            }
+            return 21;
+
+            
+        }
+
+        
+    }
+}
